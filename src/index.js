3e60ea055c717a54386d863fefd24b30d22e255f
Ishaan Vadhan <84273725+IshaanVadhan@users.noreply.github.com>
2022-08-12 02:03:13 +0530

    signin signup auth

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import App from './App';
 import { GlobalStyles } from './global-styles';
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{firebase}}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>
 );
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,8 +1,16 @@
 import Firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import { seedDatabase } from "../seed";
 
-const config = {};
+const config = {
+    apiKey: "AIzaSyAa3JcFJcvWAf3I2nyrOqDc1Dks-jqZ-fQ",
+    authDomain: "netflix-clone-iv2022.firebaseapp.com",
+    projectId: "netflix-clone-iv2022",
+    storageBucket: "netflix-clone-iv2022.appspot.com",
+    messagingSenderId: "516082721739",
+    appId: "1:516082721739:web:b1d7fee7954ef15d3a4c4e"
+};
 
 const firebase = Firebase.initializeApp(config);
 
